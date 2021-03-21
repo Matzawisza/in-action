5ef268adaa733158ae6187349312a8c53201eba9
Mateusz Zawisza <mat.zawisza@gmail.com>
2021-03-21 10:57:52 +0100

    Inserting my API key from YELP

--- apis/api_key.R
+++ apis/api_key.R
@@ -1,2 +1,8 @@
 # Store your yelp key in this variable
-yelp_key <- "YOUR_YELP_KEY"
+yelp_key <- "wdpT3fyBlqUYrhnyWOrxfVHFCFGGs3_MOMdXjkYAHRXuCzzVmg7ODwGAIHH9fz8clUy0t-wkrKhRl26SFaEb8gJUqwNHNyMpGTcgeJhxqmfsfxHoL9-t6sreKBhXYHYx"
+
+# Client ID
+# q2kbPhMNdCZj99iSm1U6nw
+# 
+# API Key
+# wdpT3fyBlqUYrhnyWOrxfVHFCFGGs3_MOMdXjkYAHRXuCzzVmg7ODwGAIHH9fz8clUy0t-wkrKhRl26SFaEb8gJUqwNHNyMpGTcgeJhxqmfsfxHoL9-t6sreKBhXYHYx
